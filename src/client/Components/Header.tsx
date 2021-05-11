1456a1cbca63ee5299a46316bb846441e36cc11e
FRickReich <frederikreich@googlemail.com>
2021-05-11 14:29:52 +0200

    Add User home route to frontend

--- src/client/Components/Header.tsx
+++ src/client/Components/Header.tsx
@@ -8,7 +8,7 @@ const Header : React.FunctionComponent = () : React.ReactElement =>
                 <li><a href={'/'}>Back</a></li>
                 <li><a href={`/${ process.env.SERVICE_NAME }/api/test`}>Dashboard Api test</a></li>
                 <li><a href={`/${ process.env.SERVICE_NAME }/`}>Dashboard home</a></li>
-                <li><a href={`/${ process.env.SERVICE_NAME }/`}>User home</a></li>
+                <li><a href={`/${ process.env.SERVICE_NAME }/user/`}>User home</a></li>
                 <li><a href={`/${ process.env.SERVICE_NAME }/public`}>Public page</a></li>
                 <li><a href={`/${ process.env.SERVICE_NAME }/error`}>Error page</a></li>
             </ul>
